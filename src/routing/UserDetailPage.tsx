723fc6e7c036f5a6c8fb8ca302b0e431552244f7
jhan25 <36556618+jhan25@users.noreply.github.com>
2024-07-11 00:45:21 +0900

    add some routing logic

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+  const params = useParams();
+
+  return <p>User {params.id}</p>;
 };
 
 export default UserDetailPage;
